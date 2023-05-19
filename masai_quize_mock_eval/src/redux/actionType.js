34d5524a832bf09b2fa42c3dec56158c37b6489b
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-19 13:19:07 +0530

    Action type

--- masai_quize_mock_eval/src/redux/actionType.js
+++ masai_quize_mock_eval/src/redux/actionType.js
@@ -1,2 +1,2 @@
-export  const USER__DATA__POST = "type/user/data/post"
-export  const USER__DATA__GET = "type/user/data/get"
+export  const USER__DATA__POST = "type/user/data/post";
+export  const USER__DATA__GET = "type/user/data/get";
