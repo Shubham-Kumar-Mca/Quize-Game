01db603dfcea64b677dde6209bf28a2c701f7587
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-05-19 13:22:35 +0530

    Quize Page

--- masai_quize_mock_eval/src/pages/quize/Quize.jsx
+++ masai_quize_mock_eval/src/pages/quize/Quize.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import "./quize.css"
+import "./quize.css";
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
